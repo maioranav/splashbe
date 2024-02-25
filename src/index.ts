b760829825e80662997bbf6b475fff5e3319b7f7
Vincenzo Maiorana <vincenzo.maiorana@intellisync.it>
2024-02-25 14:18:56 +0100

    refactor: change debug use in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@ import { AppDataSource } from "./config/data-source.config";
 import API from "./model/API";
 import { AutoLoad } from "./config/autoload.config";
 import { apiConfig } from "./config/app.config";
+import { debug } from "./utils/debug.util";
 
 const api = new API(apiConfig);
 
 AppDataSource.initialize()
    .then(() => {
-      console.log("Data Source has been initialized!");
+      debug("Data Source has been initialized!", {data: "", status: "success"});
       AutoLoad();
    })
    .catch((err) => {
-      console.error("Error during Data Source initialization:", err);
+      debug("Error while initializing DataSource: ", {data: err, status: "error"});
    });
 
 //Run API server
