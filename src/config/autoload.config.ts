18317e135e9fdfdf3621971a0cdbed6b9d815264
Vincenzo M <vincenzomaiorana@gmail.com>
2024-03-16 09:24:51 +0100

    feat(chore): autocreate nonce if missing

--- src/config/autoload.config.ts
+++ src/config/autoload.config.ts
@@ -1,6 +1,9 @@
 import { AppDataSource } from "./data-source.config";
 import { Admin } from "../model/Admin";
 import AdminService from "../services/admin.service";
+import { MainConf } from "../model/MainConf";
+import { MainType } from "../model/mainconf.enum";
+import { randomUUID } from "crypto";
 
 export const AutoLoad = async () => {
    const mainAdmin = new Admin();
@@ -9,6 +12,7 @@ export const AutoLoad = async () => {
       process.env.TYPEORM_PASSWORD ?? "password"
    );
 
+   // Autoconfig for the admin user if it doesn't exist yet
    try {
       AppDataSource.transaction(async (manager) => {
          const isSaved = await manager.findOneBy(Admin, { username: mainAdmin.username });
@@ -16,6 +20,19 @@ export const AutoLoad = async () => {
 
          isSaved.password = mainAdmin.password;
          manager.save(isSaved);
+      });
+   } catch (e) {
+      console.error(e);
+   }
+
+   // Autoconfig for nonce generation
+   try {
+      AppDataSource.transaction(async (manager) => {
+         const isSaved = await manager.findOneBy(MainConf, { title: MainType.FESECRET });
+         if (!isSaved)
+            return manager.save(
+               manager.create(MainConf, { title: MainType.FESECRET, data: randomUUID() })
+            );
       });
    } catch (e) {
       console.error(e);
